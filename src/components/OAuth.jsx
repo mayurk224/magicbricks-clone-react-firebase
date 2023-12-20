b33ca57f963657a9e813d2abc8648a56986baa71
Mayur Kamble <mayurkamble0250@gmail.com>
2023-12-20 20:49:18 +0530

    Completed OAuth Functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import {GoogleAuthProvider, getAuth, signInWithEmailAndPassword, signInWithPopup} from "firebase/auth"
+import {doc, getDoc, serverTimestamp, setDoc} from "firebase/firestore"
+import {db} from "../firebase"
+import { useNavigate } from "react-router";
 
 export default function OAuth() {
-  return (
-    <button
+  const navigate = useNavigate();
+  async function onGoogleClick(){
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
       
-      className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    >
+      const docRef = doc(db,"users",user.uid);
+      const docSnap = await getDoc(docRef);
+      if(!docSnap.exists()){
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+          timestamp: serverTimestamp(),
+        })
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("Cant authorise user");
+    }
+  }
+  return (
+    <button type="button" onClick={onGoogleClick} className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
       <FcGoogle className="text-xl mr-2" />
       Go on With Google
     </button>
